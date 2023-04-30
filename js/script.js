0f6f2646ab68f0a5733590306f7a206f505faa0b
Niloy Deb <124151536+niloydeb02@users.noreply.github.com>
2023-04-30 21:53:37 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,27 @@
+
+const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
+
+
+const toastTrigger = document.getElementById('liveToastBtn')
+const toastLiveExample = document.getElementById('liveToast')
+
+if (toastTrigger) {
+  const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample)
+  toastTrigger.addEventListener('click', () => {
+    toastBootstrap.show()
+  })
+}
+
+
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+
+
+"focus-ring": (
+  css-var: true,
+  css-variable-name: focus-ring-color,
+  class: focus-ring,
+  values: map-loop($theme-colors-rgb, rgba-css-var, "$key", "focus-ring")
+),
